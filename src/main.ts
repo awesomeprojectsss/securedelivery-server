c875636699bb2fa2a26b46c90d31af202a481917
Lucas Silva <lucasnathandev@proton.me>
2026-09-15 22:11:31 -0300

    chore: add Prisma ORM and skills

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      instrument: ObserveInstrument,
+    },
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
